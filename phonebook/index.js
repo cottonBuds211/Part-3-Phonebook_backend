be7bce638e8baaf6fabcd398e010627cd0db3a9c
Cotton <johhnythepogi@gmail.com>
2024-07-27 08:26:29 +0800

    mongo db backend

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,35 +1,36 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const cors = require("cors");
-
-let persons = [
-	{
-		id: "1",
-		name: "Arto Hellas",
-		number: "040-123456",
-	},
-	{
-		id: "2",
-		name: "Ada Lovelace",
-		number: "39-44-5323523",
-	},
-	{
-		id: "3",
-		name: "Dan Abramov",
-		number: "12-43-234345",
-	},
-	{
-		id: "4",
-		name: "Mary Poppendieck",
-		number: "39-23-6423122",
-	},
-	{
-		id: "5",
-		name: "Jimmy Poppendieck",
-		number: "39-23-6423124",
-	},
-];
+const Person = require("./models/persons");
+// let persons = [
+// 	{
+// 		id: "1",
+// 		name: "Arto Hellas",
+// 		number: "040-123456",
+// 	},
+// 	{
+// 		id: "2",
+// 		name: "Ada Lovelace",
+// 		number: "39-44-5323523",
+// 	},
+// 	{
+// 		id: "3",
+// 		name: "Dan Abramov",
+// 		number: "12-43-234345",
+// 	},
+// 	{
+// 		id: "4",
+// 		name: "Mary Poppendieck",
+// 		number: "39-23-6423122",
+// 	},
+// 	{
+// 		id: "5",
+// 		name: "Jimmy Poppendieck",
+// 		number: "39-23-6423124",
+// 	},
+// ];
 
 const generateId = () => {
 	const id = Math.floor(Math.random() * 2000);
@@ -50,30 +51,32 @@ app.get("/", (request, response) => {
 });
 
 app.get("/api/persons", (request, response) => {
-	response.json(persons);
+	Person.find({}).then((persons) => {
+		response.json(persons);
+	});
 });
-app.get("/info", (request, response) => {
-	const date = new Date();
-	const info = `<p>Phone book has info for ${persons.length} people <br /> ${date} </p>`;
+// app.get("/info", (request, response) => {
+// 	const date = new Date();
+// 	const info = `<p>Phone book has info for ${persons.length} people <br /> ${date} </p>`;
 
-	response.send(info).end();
-});
+// 	response.send(info).end();
+// });
 
-app.get("/api/persons/:id", (request, response) => {
-	const id = request.params.id;
-	const person = persons.find((person) => person.id === id);
-	if (person) {
-		response.json(person);
-	} else {
-		response.status(404).end();
-	}
-});
+// app.get("/api/persons/:id", (request, response) => {
+// 	const id = request.params.id;
+// 	const person = persons.find((person) => person.id === id);
+// 	if (person) {
+// 		response.json(person);
+// 	} else {
+// 		response.status(404).end();
+// 	}
+// });
 
-app.delete("/api/persons/:id", (request, response) => {
-	const id = request.params.id;
-	persons = persons.filter((person) => person.id !== id);
-	response.status(204).end();
-});
+// app.delete("/api/persons/:id", (request, response) => {
+// 	const id = request.params.id;
+// 	persons = persons.filter((person) => person.id !== id);
+// 	response.status(204).end();
+// });
 
 app.post("/api/persons", (request, response) => {
 	const body = request.body;
